af50a8a19858b3b4ab75448448e48d2bb3d56d42
ZunainAliAzam <zunainazam456@gmail.com>
2024-02-17 02:47:21 +0500

    navigation bar commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
+import "./App.css"
+
 const App = () => {
     return (
         <div>
+            <nav>
+                <div className="logo">
+                    <img src="/images/brand_logo.png" alt="brand logo" />
+                </div>
 
+                <ul>
+                    <li>
+                        <a href="/home">Home</a>
+                    </li>
+                    <li>
+                        <a href="/menu">Menu</a>
+                    </li>
+                    <li>
+                        <a href="/location">Location</a>
+                    </li>
+                    <li>
+                        <a href="/about">About</a>
+                    </li>
+                    <li>
+                        <a href="contact">Contact</a>
+                    </li>
+                </ul>
+
+                <button>Login</button>
+            </nav>
 
         </div>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from '../src/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode> 
-    hello world
     <App/>
   </React.StrictMode>,
 )
