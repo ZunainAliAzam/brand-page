b707a2df6e3163218c94114102b88b83ec7cedd8
ZunainAliAzam <zunainazam456@gmail.com>
2024-02-19 02:28:29 +0500

    commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
           <a href="/about">About</a>
         </li>
         <li>
-          <a href="contact">Contact</a>
+          <a href="/contact">Contact</a>
         </li>
       </ul>
 
